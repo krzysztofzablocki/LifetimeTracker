aa3debb77c18ae3df0fcdaaa2f1d3f591af7a828
Hans Seiffert <dienstefach@posteo.de>
2017-11-10 01:54:01 +0100

    [Example] Rename Example project to "Example" to avoid confusion to the actual LifetimeTracker project

--- Example/Pods/Target Support Files/Pods-Example/Pods-Example-umbrella.h
+++ Example/Pods/Target Support Files/Pods-Example/Pods-Example-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_LifetimeTracker_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_LifetimeTracker_TestsVersionString[];
+FOUNDATION_EXPORT double Pods_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ExampleVersionString[];
 
--- Example/Pods/Target Support Files/Pods-LifetimeTracker_Tests/Pods-LifetimeTracker_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LifetimeTracker_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_LifetimeTracker_Tests
-@end
