95a1c80435faf9036648ab3a85f8408dcca7a4f5
Hans Seiffert <dienstefach@posteo.de>
2019-03-26 18:46:31 +0100

    Issue #45 Example: Failing logic if screen is reentered

---
+++ Example/LifetimeTracker/NestedVCExample.swift
@@ -0,0 +1,41 @@
+//
+//  NestedVCExample.swift
+//  Example
+//
+//  Created by Hans Seiffert on 26.03.19.
+//  Copyright © 2019 LifetimeTracker. All rights reserved.
+//
+
+import UIKit
+import LifetimeTracker
+
+class BaseViewController: UIViewController, LifetimeTrackable {
+	
+	static var lifetimeConfiguration = LifetimeConfiguration(maxCount: 1)
+	
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+		trackLifetime()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		trackLifetime()
+	}
+}
+
+class ChildAViewController: BaseViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .yellow
+	}
+}
+
+class ChildBViewController: BaseViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .blue
+	}
+}
