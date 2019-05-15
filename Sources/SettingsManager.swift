31d108e34d89a2866f958d765178205ee745faa7
Philippe Casgrain <philippe.casgrain@lightspeedhq.com>
2019-05-15 14:59:56 -0400

    Prevent crash on iPad
    We’ll crash with UIAlertControllerStyleActionSheet on iPad, so we work around
    with a root-less popover for style UIModalPresentationPopover.

--- Sources/SettingsManager.swift
+++ Sources/SettingsManager.swift
@@ -10,6 +10,17 @@ import UIKit
 
 struct SettingsManager {
 
+    // On iPhone, this has no effect if the alert has preferredStyle: .actionSheet.
+    // On iPad, this creates a root-less popover which mimics the appearance of an actionSheet
+    // without requiring a sourceView or barButton.
+    private static func createRootlessPopover(centeredOn view: UIView, alert: UIAlertController) {
+        if let popoverController = alert.popoverPresentationController {
+            popoverController.sourceView = view
+            popoverController.sourceRect = CGRect(x: view.bounds.midX, y: view.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
+    }
+
 	static func showSettingsActionSheet(on viewController: UIViewController, completionHandler: @escaping (HideOption) -> Void) {
         let alert = UIAlertController(title: "settings".lt_localized, message: nil, preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "settings.hide.title".lt_localized, style: .default, handler: { (action: UIAlertAction) in
@@ -26,11 +37,15 @@ struct SettingsManager {
             alert.addAction(UIAlertAction(title: "settings.cancel".lt_localized, style: .cancel, handler: { (action: UIAlertAction) in
                 completionHandler(.none)
             }))
+            createRootlessPopover(centeredOn: viewController.view, alert: alert)
             viewController.present(alert, animated: true, completion: nil)
         }))
         alert.addAction(UIAlertAction(title: "settings.cancel".lt_localized, style: .cancel, handler: { (action: UIAlertAction) in
             completionHandler(.none)
         }))
+
+        createRootlessPopover(centeredOn: viewController.view, alert: alert)
+
         viewController.present(alert, animated: true, completion: nil)
     }
 }
