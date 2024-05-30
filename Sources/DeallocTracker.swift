krzysztofzablocki/LifetimeTracker

commit f5c25c5bb152892df9b333b16c6b74fe7060ab55

chore: cleanup

Sources/DeallocTracker.swift
@@ -27,5 +27,8 @@ fileprivate final class DeallocTracker {
 ///   - closure: Closure to execute.
 internal func onDealloc(of owner: Any, closure: @escaping () -> Void) {
     var tracker = DeallocTracker(onDealloc: closure)
-    objc_setAssociatedObject(owner, &tracker, tracker, .OBJC_ASSOCIATION_RETAIN)
+    withUnsafePointer(to: tracker) { pointer in
+        var tracker = pointer
+        objc_setAssociatedObject(owner, &tracker, tracker, .OBJC_ASSOCIATION_RETAIN)
+    }
 }

LifetimeTracker.podspec
@@ -9,8 +9,8 @@ Pod::Spec.new do |s|
   s.license      = { :type => "MIT", :file => "LICENSE" }
   s.author             = { "Krzysztof Zablocki" => "krzysztof.zablocki@pixle.pl" }
   s.social_media_url   = "http://twitter.com/merowing_"
-  s.ios.deployment_target = "8.0"
-  s.osx.deployment_target = '10.10'
+  s.ios.deployment_target = "12.0"
+  s.osx.deployment_target = '10.13'
   s.source       = { :git => "https://github.com/krzysztofzablocki/LifetimeTracker.git", :tag => s.version.to_s }
   s.ios.source_files  = "Sources/*.swift", "Sources/iOS/**/*.swift"
   s.macos.source_files  = "Sources/*.swift"

commit 1d1984d8e8c0b9e8b9528d05629d8b458b74253d

Fix leak detection and warning
Revert f5c25c5bb152892df9b333b16c6b74fe7060ab55 which introduced
regression (`testDeallocTrackerDoesntFire` failure).

Then fix original warning:

>Forming 'UnsafeRawPointer' to a variable of type 'DeallocTracker';
this is likely incorrect because 'DeallocTracker' may contain an
object reference.

by workaround from swift evolution proposal: https://github.com/atrick/swift-evolution/blob/diagnose-implicit-raw-bitwise/proposals/nnnn-implicit-raw-bitwise-conversion.md#associated-object-string-keys

Resolves https://github.com/krzysztofzablocki/LifetimeTracker/issues/93

Sources/DeallocTracker.swift
@@ -26,9 +26,11 @@ fileprivate final class DeallocTracker {
 ///   - owner: Owner to track.
 ///   - closure: Closure to execute.
 internal func onDealloc(of owner: Any, closure: @escaping () -> Void) {
-    var tracker = DeallocTracker(onDealloc: closure)
-    withUnsafePointer(to: tracker) { pointer in
-        var tracker = pointer
-        objc_setAssociatedObject(owner, &tracker, tracker, .OBJC_ASSOCIATION_RETAIN)
-    }
+    let tracker = DeallocTracker(onDealloc: closure)
+    objc_setAssociatedObject(owner, getID(tracker), tracker, .OBJC_ASSOCIATION_RETAIN)
+}
+
+// https://github.com/atrick/swift-evolution/blob/diagnose-implicit-raw-bitwise/proposals/nnnn-implicit-raw-bitwise-conversion.md#associated-object-string-keys
+private func getID(_ object: AnyObject) -> UnsafeRawPointer {
+    return UnsafeRawPointer(Unmanaged.passUnretained(object).toOpaque())
 }